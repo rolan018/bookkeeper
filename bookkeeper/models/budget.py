7c646486dfd55478fd965de65a37d2d659ec4069
rolan018 <dadashev.rr@phystech.edu>
2023-03-13 00:21:19 +0300

    [models] update models for view parametrs

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -1,23 +1,34 @@
 """
-Модель бюджета
+Описан класс, представляющий описание бюджета
 """
-
 from dataclasses import dataclass, field
 from datetime import datetime
+from bookkeeper.models.expense import Expense
 
 
 @dataclass(slots=True)
-class Budget:
+class Budget: # pylint: disable=too-few-public-methods
     """
-    Бюджет
-    begin_period_date - начало
-    end_period_date - конец
-    value - сумма расходов
-    pk - id в бд
+    Бюджет (совокупность расходов за период времени).
+    name - название периода
+    begin_period_date - дата начала отсчета
+    end_period_date - дата конца отсчета
+    value - сумма расходов за данный период
+    pk - id записи в базе данных
     """
 
-    begin_period: datetime = field(default_factory=datetime.now)
-    end_period: datetime = field(default_factory=datetime.now)
+    name: str = 'Период по умолчанию'
+    begin_period_date: datetime = field(default_factory=datetime.now)
+    end_period_date: datetime = field(default_factory=datetime.now)
     value: float = 0
-    limit: float = 0
     pk: int = 0
+
+    def calculate(self, data: list[Expense]) -> float:
+        """
+        Function calculates all expanses for given period"""
+        tmp = 0.
+        for element in data:
+            if element.expense_date <= self.end_period_date:
+                if element.expense_date >= self.begin_period_date:
+                    tmp += float(element.amount)
+        return tmp
